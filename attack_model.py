869d805c259846dd37ddb15cf54fcfac595b12be
chrissweet <csweet1@nd.edu>
2022-05-23 21:32:48 -0400

    reformat + output for attack.

--- attack_model.py
+++ attack_model.py
@@ -32,6 +32,8 @@ from utils import *
 
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
+
+
 def remove_module_state_dict(state_dict):
     new_state_dict = OrderedDict()
     for key in state_dict.keys():
@@ -39,20 +41,22 @@ def remove_module_state_dict(state_dict):
         new_state_dict[new_key] = state_dict[key]
     return new_state_dict
 
+
 # ------------------------------------------------------------------------------
 parser = argparse.ArgumentParser()
 # training
 parser.add_argument('--dataset', type=str, default='cifar')
 parser.add_argument('--batch_size', type=int, default=50)
-parser.add_argument("--norm", type=str, default=None, choices=[None, "norm", "batch", "instance", "layer", "act"])
+parser.add_argument("--norm", type=str, default=None,
+                    choices=[None, "norm", "batch", "instance", "layer", "act"])
 
 # EBM specific
 parser.add_argument("--n_steps", type=int, default=100)
 parser.add_argument("--width", type=int, default=10)
 parser.add_argument("--depth", type=int, default=28)
-# 
+#
 parser.add_argument('--n_steps_refine', type=int, default=0)
-parser.add_argument('--n_classes',type=int,default=10)
+parser.add_argument('--n_classes', type=int, default=10)
 parser.add_argument('--init_batch_size', type=int, default=128)
 parser.add_argument('--softmax_ce', action='store_true')
 # attack
@@ -70,48 +74,51 @@ parser.add_argument("--sgld_sigma", type=float, default=1e-2)
 parser.add_argument("--n_dup_chains", type=int, default=5)
 parser.add_argument("--sigma", type=float, default=.03)
 parser.add_argument("--base_dir", type=str, default='./adv_results')
+parser.add_argument("--attack_tries", type=int, default=20)
 
 # logging
 
-parser.add_argument('--exp_name', type=str, default='exp', help='saves everything in ?r/exp_name/')
+parser.add_argument('--exp_name', type=str, default='exp',
+                    help='saves everything in ?r/exp_name/')
 args = parser.parse_args()
 device = torch.device('cuda')
 args_ = vars(args)
 for key in args_.keys():
-    print('{}:   {}'.format(key,args_[key]))
+    print('{}:   {}'.format(key, args_[key]))
 
 
 base_dir = args.base_dir
 
 save_dir = os.path.join(base_dir, args.exp_name, 'saved_model')
-last_dir = os.path.join(save_dir,'last')
-best_dir = os.path.join(save_dir,'best')
-data_dir = os.path.join(base_dir,'data')
+last_dir = os.path.join(save_dir, 'last')
+best_dir = os.path.join(save_dir, 'best')
+data_dir = os.path.join(base_dir, 'data')
 
 
 class gradient_attack_wrapper(nn.Module):
-  def __init__(self, model):
-    super(gradient_attack_wrapper, self).__init__()
-    self.model = model.eval()
+    def __init__(self, model):
+        super(gradient_attack_wrapper, self).__init__()
+        self.model = model.eval()
 
-  def forward(self, x):
-    x = x - 0.5
-    x = x / 0.5
-    x.requires_grad_()
-    out = self.model.module.refined_logits(x)
-    return out
+    def forward(self, x):
+        x = x - 0.5
+        x = x / 0.5
+        x.requires_grad_()
+        out = self.model.module.refined_logits(x)
+        return out
 
-  def eval(self):
-    return self.model.eval()
+    def eval(self):
+        return self.model.eval()
 
-model_attack_wrapper =gradient_attack_wrapper
 
-transformer_train  = transforms.Compose([transforms.ToTensor()])
-transformer_test  = transforms.Compose([transforms.ToTensor()])
+model_attack_wrapper = gradient_attack_wrapper
 
-data_loader  = torch.utils.data.DataLoader(datasets.CIFAR10(data_dir, train=False,
-                                                            transform=transformer_test, download=True),
-                                           batch_size=args.batch_size, shuffle=False, num_workers=10)
+transformer_train = transforms.Compose([transforms.ToTensor()])
+transformer_test = transforms.Compose([transforms.ToTensor()])
+
+data_loader = torch.utils.data.DataLoader(datasets.CIFAR10(data_dir, train=False,
+                                                           transform=transformer_test, download=True),
+                                          batch_size=args.batch_size, shuffle=False, num_workers=10)
 init_loader = torch.utils.data.DataLoader(datasets.CIFAR10(data_dir, train=True,
                                                            download=True, transform=transformer_train),
                                           batch_size=args.init_batch_size, shuffle=True, num_workers=1)
@@ -144,6 +151,7 @@ class CCF(F):
         else:
             return torch.gather(logits, 1, y[:, None])
 
+
 # construct model and ship to GPU
 f = CCF(args.depth, args.width, args.norm)
 print(args.load_path)
@@ -157,6 +165,8 @@ else:
     f.load_state_dict(ckpt_dict)
 
 # wrapper class to provide utilities for what you need
+
+
 class DummyModel(nn.Module):
     def __init__(self, f):
         super(DummyModel, self).__init__()
@@ -167,7 +177,8 @@ class DummyModel(nn.Module):
 
     def refined_logits(self, x, n_steps=args.n_steps_refine):
         xs = x.size()
-        dup_x = x.view(xs[0], 1, xs[1], xs[2], xs[3]).repeat(1, args.n_dup_chains, 1, 1, 1)
+        dup_x = x.view(xs[0], 1, xs[1], xs[2], xs[3]).repeat(
+            1, args.n_dup_chains, 1, 1, 1)
         dup_x = dup_x.view(xs[0] * args.n_dup_chains, xs[1], xs[2], xs[3])
         dup_x = dup_x + torch.randn_like(dup_x) * args.sigma
         refined = self.refine(dup_x, n_steps=n_steps, detach=False)
@@ -190,14 +201,16 @@ class DummyModel(nn.Module):
         x_k = torch.autograd.Variable(x, requires_grad=True) if detach else x
         # sgld
         for k in range(n_steps):
-            f_prime = torch.autograd.grad(self.f(x_k).sum(), [x_k], retain_graph=True)[0]
+            f_prime = torch.autograd.grad(
+                self.f(x_k).sum(), [x_k], retain_graph=True)[0]
             x_k.data += f_prime + args.sgld_sigma * torch.randn_like(x_k)
         final_samples = x_k.detach() if detach else x_k
         return final_samples
 
     def grad_norm(self, x):
         x_k = torch.autograd.Variable(x, requires_grad=True)
-        f_prime = torch.autograd.grad(self.f(x_k).sum(), [x_k], retain_graph=True)[0]
+        f_prime = torch.autograd.grad(
+            self.f(x_k).sum(), [x_k], retain_graph=True)[0]
         grad = f_prime.view(x.size(0), -1)
         return grad.norm(p=2, dim=1)
 
@@ -219,51 +232,59 @@ model = f.to(device)
 model = nn.DataParallel(model).to(device)
 
 model.eval()
-## Define criterion
+# Define criterion
 criterion = foolbox.criteria.Misclassification()
 
-## Initiate attack and wrap model
+# Initiate attack and wrap model
 model_wrapped = model_attack_wrapper(model)
-fmodel = foolbox.models.PyTorchModel(model_wrapped, bounds=(0.,1.), num_classes=10, device=device)
+fmodel = foolbox.models.PyTorchModel(
+    model_wrapped, bounds=(0., 1.), num_classes=10, device=device)
 
 if args.distance == 'L2':
     distance = foolbox.distances.MeanSquaredDistance
-    attack = foolbox.attacks.L2BasicIterativeAttack(model=fmodel, criterion=criterion, distance=distance)
+    attack = foolbox.attacks.L2BasicIterativeAttack(
+        model=fmodel, criterion=criterion, distance=distance)
 else:
     distance = foolbox.distances.Linfinity
-    attack = foolbox.attacks.RandomStartProjectedGradientDescentAttack(model=fmodel, criterion=criterion, distance=distance)
+    attack = foolbox.attacks.RandomStartProjectedGradientDescentAttack(
+        model=fmodel, criterion=criterion, distance=distance)
 
 print('Starting...')
 for i, (img, label) in enumerate(data_loader):
+    print("i... ", i)
     adversaries = []
     if i < args.start_batch:
         continue
     if i >= args.end_batch:
-      break
+        break
     img = img.data.cpu().numpy()
     logits = model_wrapped(torch.from_numpy(img[:, :, :, :]).to(device))
-    _, top = torch.topk(logits,k=2,dim=1)
+    _, top = torch.topk(logits, k=2, dim=1)
     top = top.data.cpu().numpy()
-    pred = top[:,0]
+    pred = top[:, 0]
     for k in range(len(label)):
-      im = img[k,:,:,:]
-      orig_label = label[k].data.cpu().numpy()
-      if pred[k] != orig_label:
-        continue
-      best_adv = None
-      for ii in range(20):
-          try:
-            adversarial = attack(im, label=orig_label, unpack=False, random_start=True, iterations=args.n_steps_pgd_attack) 
-            if ii == 0 or best_adv.distance > adversarial.distance:
-                best_adv = adversarial
-          except:
+        print("k... ", k)
+        im = img[k, :, :, :]
+        orig_label = label[k].data.cpu().numpy()
+        if pred[k] != orig_label:
             continue
-      try:
-          adversaries.append((im, orig_label, adversarial.image, adversarial.adversarial_class))
-      except:
-          continue
+        best_adv = None
+        for ii in range(args.attack_tries):
+            print("ii... ", ii)
+            try:
+                adversarial = attack(im, label=orig_label, unpack=False,
+                                     random_start=True, iterations=args.n_steps_pgd_attack)
+                if ii == 0 or best_adv.distance > adversarial.distance:
+                    best_adv = adversarial
+            except:
+                continue
+        try:
+            adversaries.append(
+                (im, orig_label, adversarial.image, adversarial.adversarial_class))
+        except:
+            continue
     adv_save_dir = os.path.join(base_dir, args.exp_name)
     save_file = 'adversarials_batch_'+str(i)
-    if not os.path.exists(os.path.join(adv_save_dir,save_file)):
-        os.makedirs(os.path.join(adv_save_dir,save_file))
-    np.save(os.path.join(adv_save_dir,save_file),adversaries)
+    if not os.path.exists(os.path.join(adv_save_dir, save_file)):
+        os.makedirs(os.path.join(adv_save_dir, save_file))
+    np.save(os.path.join(adv_save_dir, save_file), adversaries)
